01c8677a39e6e81d41014152cbf91f86e9c52011
Aurelien Perraudin <orelien.perraudin@gmail.com>
2019-10-17 16:07:03 +0200

    atelier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MenuComponent } from './menu/menu.component';
 import { ROUTES } from './app.route';
 import { RouterModule } from '@angular/router';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { SignUpComponent } from './sign-up/sign-up.component';
     DisplayFilmsDirective,
     ArticleComponent,
     MenuComponent,
-    SignUpComponent
+    SignUpComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
